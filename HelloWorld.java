a1a0733d59285165ba5fc4ac9e2a0516e8b85329
Ashesh <ashesh01@gmail.com>
2021-09-08 12:52:31 +0000

    Modification in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,12 @@ class HelloWorld
 {
 	public static void main(String[] args){
 		System.out.println("Welcome to the world of Java");
+		System.out.println("You can see the addition of two numbers below:");
+		int a,b,c;
+		a = 10;
+		b = 20;
+		c = a+b;
+		
+		System.out.println("Addition of two numbers is : "+c);
 	}
 }
